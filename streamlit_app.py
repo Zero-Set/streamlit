c00f07ef95aaf7713f07664e83c65d17263088f9
0_set <9507468+Zero-Set@users.noreply.github.com>
2023-02-04 21:48:23 +0900

    upd

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,9 +35,12 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
+QUERY="SELECT * FROM FRUIT_LOAD_LIST"
+MSG="The fruit load list contains:"
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute(QUERY)
 my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
+streamlit.text(MSG)
 streamlit.text(my_data_row)
