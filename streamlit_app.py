0ded60020ec75bf40b29230d2b50557dac70c4cf
0_set <9507468+Zero-Set@users.noreply.github.com>
2023-02-04 22:21:51 +0900

    upd

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,12 +29,16 @@ HEADER_TEXT = 'Fruitvice Fruit Advice!'
 
 streamlit.header(HEADER_TEXT)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("please select...")  
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 QUERY="SELECT * FROM FRUIT_LOAD_LIST"
 MSG="The fruit load list contains:"
