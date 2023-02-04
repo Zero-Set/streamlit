b0b902b35066e3394ddbade92f469f989e4cc287
0_set <9507468+Zero-Set@users.noreply.github.com>
2023-02-04 19:38:56 +0900

    upd

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ QUESTION_TEXT = 'What fruit would you like information about?'
 
 streamlit.header(HEADER_TEXT)
 
-fruit_choice = stremalit.text_input(QUESTIION_TEXT,'Kiwi')
+fruit_choice = stremalit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered',fruit_choice)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
