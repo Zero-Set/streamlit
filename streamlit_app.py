58f3c61c7c8351b5beb4ee16430556be6f827430
0_set <9507468+Zero-Set@users.noreply.github.com>
2023-02-04 23:07:59 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,46 +29,16 @@ HEADER_TEXT = 'Fruitvice Fruit Advice!'
 QUERY="SELECT * FROM FRUIT_LOAD_LIST"
 MSG="The fruit load list contains:"
 
-# create the repeatable code block (called a function)
-def get_fruityvice_data(this_fruit_choice):
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-  fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-  return fruityvice_normalized
-
-def insert_row_snowflake(new_fruit):
-  with my_cnx.cursor() as my_cur:
-    my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-    return "Thanks for adding " + new_fruit
-
-streamlit.header(HEADER_TEXT)
-
-try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-    streamlit.error("please select...")  
-  else:
-    back_from_function = get_fruityvice_data(fruit_choice)
-    streamlit.dataframe(back_from_function)
-except URLError as e:
-  streamlit.error()
 
 
-# don't run anything past here while we troubleshoot
-streamlit.stop()
+
+
+
+
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute(QUERY)
-my_data_rows = my_cur.fetchall()
-streamlit.text(MSG)
-streamlit.dataframe(my_data_rows)
-
-add_my_fruit = streamlit.text_input('What fruit would you liketo add?','jackfruit')
-
-if streamlit.button('Add a Fruit to the List'):
-  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-  back_from_function = insert_row_snowflake(add_my_fruit)
-  streamlit.text(back_from_function)
-  streamlit.write('Thanks for adding jackfruit', add_my_fruit)
-
-
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
