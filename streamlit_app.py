0892a2b18c981cd5e87f913c1e2a08c3ff364b95
0_set <9507468+Zero-Set@users.noreply.github.com>
2023-02-04 19:14:28 +0900

    upd2

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,4 +29,4 @@ streamlit.header(HEADER_TEXT)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
