ab83b0b7ec547ffdad2e97565140d5ecb14bb7e4
0_set <9507468+Zero-Set@users.noreply.github.com>
2023-02-04 23:20:14 +0900

    comment out

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,25 +6,25 @@ from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Favorites')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
+#streamlit.header('Breakfast Favorites')
+#streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+#streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+#streamlit.text('🐔Hard-Boiled Free-Range Egg')
+#streamlit.text('🥑🍞 Avocado Toast')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
+#streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+#my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+#my_fruit_list = my_fruit_list.set_index('Fruit')
 
-pre_populate_list = ['Avocado','Strawberries']
+#pre_populate_list = ['Avocado','Strawberries']
 
 # let's put a pick list here so they can pick the fruit they want to include
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),pre_populate_list)
-fruits_to_show = my_fruit_list.loc[fruits_selected]
+#fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),pre_populate_list)
+#fruits_to_show = my_fruit_list.loc[fruits_selected]
 # New Section to display fruityvice api response
-streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('what fruit ...','Kiwi')
-streamlit.write('The user entered',fruit_choice)
+#streamlit.header('Fruityvice Fruit Advice!')
+#fruit_choice = streamlit.text_input('what fruit ...','Kiwi')
+#streamlit.write('The user entered',fruit_choice)
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit" + fruit_choice)
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit" + fruit_choice)
