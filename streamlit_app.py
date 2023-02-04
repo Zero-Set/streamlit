cd5dcc95a4d48c5b9123dade9af134a2b7646703
0_set <9507468+Zero-Set@users.noreply.github.com>
2023-02-04 23:40:47 +0900

    rollback

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,5 @@
 import streamlit
 import pandas as pd
-import requests
-import snowflake.connector
-from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -21,14 +18,6 @@ pre_populate_list = ['Avocado','Strawberries']
 # let's put a pick list here so they can pick the fruit they want to include
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),pre_populate_list)
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-# New Section to display fruityvice api response
-streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('what fruit ...','Kiwi')
-streamlit.write('The user entered',fruit_choice)
 
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit" + fruit_choice)
-# take the json version ...
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output it the screen as a table
-streamlit.dataframe(fruityvice_normalized)
+# display the table on the page
+streamlit.dataframe(fruits_to_show)
