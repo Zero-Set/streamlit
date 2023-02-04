0e4894b7140542f460f4ae53d1736124a191935a
0_set <9507468+Zero-Set@users.noreply.github.com>
2023-02-04 22:08:21 +0900

    upd

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas as pd
 import requests
 import snowflake.connector
+import urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -37,6 +38,9 @@ streamlit.dataframe(fruityvice_normalized)
 
 QUERY="SELECT * FROM FRUIT_LOAD_LIST"
 MSG="The fruit load list contains:"
+
+# don't run anything past here while we troubleshoot
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
