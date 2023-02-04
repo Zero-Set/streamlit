5cab6184d9fc66b19c5dafbfbc6e0bf28d05d5a4
0_set <9507468+Zero-Set@users.noreply.github.com>
2023-02-04 23:12:10 +0900

    rollback lesson9 variables in statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,24 +21,10 @@ pre_populate_list = ['Avocado','Strawberries']
 # let's put a pick list here so they can pick the fruit they want to include
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),pre_populate_list)
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-# display the table on the page
-streamlit.dataframe(fruits_to_show)
-
-HEADER_TEXT = 'Fruitvice Fruit Advice!'
-QUERY="SELECT * FROM FRUIT_LOAD_LIST"
-MSG="The fruit load list contains:"
+# New Section to display fruityvice api response
+streamlit.header('Fruityvice Fruit Advice!')
+fruit_choice = streamlit.text_input('what fruit ...','Kiwi')
+streamlit.write('The user entered',fruit_choice)
 
 
-
-
-
-
-
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit" + fruit_choice)
